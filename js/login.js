3c673176efc8efc7d02905a2bb8b79da81819139
Juan Cruz Olascoaga <jolascoagaporteiro@gmail.com>
2022-08-20 00:08:10 -0300

    Login (sin oAuth 2.0)

---
+++ js/login.js
@@ -0,0 +1,37 @@
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("btn-login").addEventListener('click', function(){
+        let email = document.getElementById("email-user").value;
+        let password = document.getElementById("password-user").value;
+        let flagVar = true;
+
+        if (email == '' || email.search('@') === -1) {
+            flagVar = false;
+            showAlertError();
+        }
+
+        if (password == '') {
+            flagVar = false;
+            showAlertError();
+        }
+
+        if(flagVar) {
+            window.location.href = "home.html"
+        }
+
+
+
+    })
+
+
+
+
+
+
+})
+
+
+
